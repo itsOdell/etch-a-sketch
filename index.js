78664cee870c0a1aa10582862d060d7ab042eddb
abduladil sunnat <abduladilsunnat@gmail.com>
2020-12-18 16:43:45 -0600

    finished javascript and html but not css

---
+++ index.js
@@ -0,0 +1,31 @@
+let table = document.querySelector(".grid");
+let btn = document.querySelector(".submit");
+function clear() {
+    table.innerHTML = "";
+}
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    clear();
+    makeGrid();
+    styleColor();
+});
+function makeGrid() {
+    for (var i = 1; i < document.querySelector("#height").value; i++) {
+        var tr = document.createElement("tr");
+        tr.id = "row";  
+        table.appendChild(tr);
+
+        for (var j = 1; j < document.querySelector("#width").value; j++) {
+            var td = document.createElement("td");
+            td.id = "cell";
+            tr.appendChild(td);
+        }
+    }
+}
+function styleColor() {
+    for (var i = 0; i < (document.querySelector("#width").value) * (document.querySelector("#width").value); i++) {
+        document.querySelectorAll("#cell")[i].addEventListener("mouseover", function() {
+            this.style.backgroundColor = "black";
+        });
+    }
+}
